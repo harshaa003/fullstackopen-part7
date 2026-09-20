2d3dcc9f58b0be780f66eb9a2b9eda7c3dca76d6
harshaa003 <harshaaa905@gmail.com>
2026-09-20 23:48:55 +0530

    Complete exercise 7.8

--- bloglist/src/main.jsx
+++ bloglist/src/main.jsx
@@ -1,4 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
+)
